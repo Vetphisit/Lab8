cc36a09e177afe4eca7a63ec11aab34d18f4aefd
Vetphisit <vetpisit07@gmail.com>
2024-12-13 12:44:52 +0700

    lab8_3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,44 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    int H;
+    int B;
+
+    cout<<"Enter your age: ";
+    cin>> age;
+    if(age<=25){
+        cout<<"Enter your height: ";
+        cin>>H;
+        if(H<100){
+        cout<<"Your character = Chopper";
+        }
+        else if(H<180){
+            cout<<"Your character = Usopp";
+        }
+        else{
+            cout<<"Enter your bounty: ";
+            cin>>B;
+            if(B>1100000000){
+                cout<<"Your character = Zoro";
+            }
+            else {
+                cout<<"Your character = Sanji";
+            }
+        }
+    }
+    else if(age<=60){
+        cout<<"Enter your bounty: ";
+        cin>>B;
+        if(B> 500000000){
+            cout << "Your character = Jinbe";
+        } 
+        else {
+            cout<< "Your character = Franky";
+        }
+    }
+     else  {
+        cout << "Your character = Brook";
+    }
+}
